194a9c5681564ac6bc37090847478973678619d5
eliecer <elieaguilar91@gmail.com>
2019-10-16 22:56:13 -0500

    modificacion al xml

--- app/src/main/java/com/example/parcial2/ChatsIndividualActivity.java
+++ app/src/main/java/com/example/parcial2/ChatsIndividualActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ChatsIndividualActivity extends AppCompatActivity {
+
+    //declaracion de variables de los controles
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chat);
     }
+
+
 
 
 }
