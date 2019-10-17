8da2966e0d59e7879e630aba864ce009aeb802a2
eliecer <elieaguilar91@gmail.com>
2019-10-17 11:41:52 -0500

    modificacion al XML y Java de controles

--- app/src/main/java/com/example/parcial2/ChatsIndividualActivity.java
+++ app/src/main/java/com/example/parcial2/ChatsIndividualActivity.java
@@ -3,19 +3,27 @@ package com.example.parcial2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ChatsIndividualActivity extends AppCompatActivity {
 
     //declaracion de variables de los controles
+    private ImageView imgT;
+    private TextView tvName;
+    private TextView tvStatus;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
     }
 
-
+    private void load(){
+        tvName = (TextView)findViewById(R.id.tvName);
+    }
 
 
 }
