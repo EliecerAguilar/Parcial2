c55bb795c2c21d649f871af1c5f2ec524b40af29
eliecer <elieaguilar91@gmail.com>
2019-10-18 22:32:51 -0500

    funcional alineacion desde java preo para nuevo elemento TV

--- app/src/main/java/com/example/parcial2/ChatsIndividualActivity.java
+++ app/src/main/java/com/example/parcial2/ChatsIndividualActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class ChatsIndividualActivity extends AppCompatActivity {
@@ -25,6 +26,7 @@ public class ChatsIndividualActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         this.load();
+        this.alinear();
 
     }
 
@@ -33,10 +35,10 @@ public class ChatsIndividualActivity extends AppCompatActivity {
         tvName = (TextView)findViewById(R.id.tvName);
         tvStatus = (TextView)findViewById(R.id.tv_status);
         this.imgRedondeada(R.drawable.bat,R.id.imgB);//imagen redondeada
-
     }
 
     private void imgRedondeada(int imgGet, int imgSet){
+
         //extraemos el drawable en un bitmap
         Drawable originalDrawable = getResources().getDrawable(imgGet);
         Bitmap originalBitmap = ((BitmapDrawable) originalDrawable).getBitmap();
@@ -50,6 +52,26 @@ public class ChatsIndividualActivity extends AppCompatActivity {
         ImageView imageView = (ImageView) findViewById(imgSet);
 
         imageView.setImageDrawable(roundedDrawable);
+
+    }
+    private void alinear(){
+
+        TextView txt = new TextView(getApplicationContext());
+        txt.setId(1);
+        txt.setText("hola");
+
+
+        //obtener id del Layout donde esta el comtrol
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.RL_title);
+        //definir variable para los cambios en los parametros del control
+        RelativeLayout.LayoutParams relativeLayoutParamas;
+        //establecer la configuracion inicial del Layout
+        relativeLayoutParamas = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        //agregar regla de alinieacion del control
+        relativeLayoutParamas.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        //enviar las reglas al control
+        relativeLayout.addView(txt,relativeLayoutParamas);
 
     }
 
